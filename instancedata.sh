11c850c770a5716109b7a367d53b8e0fa8437402
EC2 Default User <ec2-user@ip-172-31-92-250.ec2.internal>
2020-03-07 02:12:57 +0000

    moved files

--- instancedata.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# creating and moving output
-
-touch metadata.txt
-
-curl -w "\n" http://169.254.169.254/latest/meta-data/hostname > metadata.txt
-curl -w "\n" http://169.254.169.254/latest/meta-data/iam/info >> metadata.txt
-curl -w "\n" http://169.254.169.254/latest/meta-data/security-groups >> metadata.txt
